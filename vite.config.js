91ebce4d46e4849b1caaeae76d8b2a5bbdf60b2d
emawid <140737336+emawid@users.noreply.github.com>
2025-08-08 18:14:47 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/interactive-rating-app/', // Replace with your GitHub repo name
+  base: '/interactive-rating-app/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-  }
-})
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        message: './message.html',
+      },
+    },
+  },
+});
